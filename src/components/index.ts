3f7e2f6297b5fd1143a8835c09c9bf06fe32a55c
nicholas matus <nicholasmatus17@gmail.com>
2026-06-25 13:34:32 -0400

    Estado Global Favoritos

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 
 
-export { Sidebar } from './Sidebar';
+export { Sidebar } from './sidebar/Sidebar';
+export { SidebarMenuItem } from './sidebar/SidebarMenuItem';
+
+export { SimpleWidget } from './dashboard/SimpleWidget';
+export { WidgetsGrid } from './dashboard/WidgetsGrid';
